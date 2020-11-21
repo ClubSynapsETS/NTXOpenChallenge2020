30dc03849937748ee4a519654f4cf036001a9c18
Samuël Lefebvre <SamLefebvre@users.noreply.github.com>
2020-11-21 00:04:41 -0500

    Add Unity Project

---
+++ Unity_NTXOpenChallenge2020/Assets/Scripts/GameUDPManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Globalization;
+
+public class GameUDPManager : MonoBehaviour
+{
+    private TextMeshProUGUI m_Text;
+    private int port;
+    private Slider m_Slider;
+    private UDPSocket socket;
+    private Toggle toggle;
+    private Slider sliderOverride;
+    public Transform platform;
+
+    public RotatorX[] rotator;
+
+    private float myoValue;
+
+    private void Awake()
+    {
+        var culture = new CultureInfo("en-US");
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 200;
+        m_Text = GameObject.Find("udp_values").GetComponent<TextMeshProUGUI>();
+        m_Slider = GameObject.Find("Slider").GetComponent<Slider>();
+        sliderOverride = GameObject.Find("SliderOverride").GetComponent<Slider>();
+        toggle = GameObject.Find("Toggle").GetComponent<Toggle>();
+
+        myoValue = 0;
+
+        ChangePort();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (toggle.isOn)
+        {
+            myoValue = sliderOverride.value;
+            m_Text.text = string.Format("Override value: {0}", sliderOverride.value.ToString("0.00"));
+
+            MovePlatform();
+            return; // exit update()
+        }
+
+        if (socket != null)
+        {
+            string data = socket.readData;
+
+            if (data == string.Empty) return;
+
+            m_Text.text = string.Format("UDP Port 5020: {0}", data);
+
+            float.TryParse(data, out float result);
+            myoValue = result;
+            m_Slider.value = result;
+        }
+
+
+        MovePlatform();
+    }
+
+    private void MovePlatform()
+    {
+        // Map Range, map the myoValue, from [0,5] to [0,1]
+        float speedRotator = Mathf.Lerp(1, 0, Mathf.InverseLerp(0, 5, myoValue));
+
+        foreach (var item in rotator)
+        {
+            item.percentSpeed = speedRotator;
+        }
+    }
+
+    public void ChangePort()
+    {
+        int.TryParse(GameObject.Find("InputField_Port").GetComponent<TMP_InputField>().text, out port);
+
+        if (socket != null)
+        {
+            socket.AbortConnection();
+        }
+
+        try
+        {
+            socket = new UDPSocket(port);
+        }
+        catch (System.Exception e)
+        {
+            m_Text.text = string.Format("Error: {0}", e.Message);
+            socket = null;
+        }
+    }
+}
---
+++ Unity_NTXOpenChallenge2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    private AudioClip clipHitWall;
+    private AudioClip clipHitPickup;
+    private AudioSource rollingSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+
+        clipHitWall = Resources.Load<AudioClip>("Sounds/wall");
+        clipHitPickup = Resources.Load<AudioClip>("Sounds/pickup");
+
+        rollingSound = gameObject.GetComponents<AudioSource>()[1];
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = String.Format("Count: {0}", count.ToString());
+        if (count >= 9)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+
+
+        rollingSound.volume = 0;
+        if (IsGrounded())
+        {
+            // Map Range
+            var volume = Mathf.Lerp(0, 1, Mathf.InverseLerp(0, 12, rb.velocity.magnitude));
+            var pitch = Mathf.Lerp(0.3f, 1, Mathf.InverseLerp(0, 12, rb.velocity.magnitude));
+            rollingSound.volume = volume;
+            rollingSound.pitch = pitch;
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 0.51f);
+    }
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            rb.AddForce(new Vector3(0.0f, 25.0f, 0.0f), ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+
+            gameObject.GetComponent<AudioSource>().PlayOneShot(clipHitPickup);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Wall"))
+        {
+            gameObject.GetComponent<AudioSource>().PlayOneShot(clipHitWall);
+        }
+
+        if (collision.collider.CompareTag("plane"))
+        {
+            Debug.Log("Game Over");
+        }
+    }
+
+}
